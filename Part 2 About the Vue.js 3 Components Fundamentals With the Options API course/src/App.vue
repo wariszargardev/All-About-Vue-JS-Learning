8001b1ce5eaf5e7c44dc7c1e831d90a3f0c267b2
warisfullstack <146804289+warisfullstack@users.noreply.github.com>
2025-04-19 21:15:18 +0500

    Basics of vue3

--- Part 2 About the Vue.js 3 Components Fundamentals With the Options API course/src/App.vue
+++ Part 2 About the Vue.js 3 Components Fundamentals With the Options API course/src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
+import ClickCounter from './components/ClickCounter.vue'
 </script>
 
 <template>
   <h1>Hello World</h1>
+  <ClickCounter />
 </template>
 
 <style scoped>
---
+++ Part 2 About the Vue.js 3 Components Fundamentals With the Options API course/src/components/ClickCounter.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+</script>
+
+<template>
+    <h1>Counter {{ count }}</h1>
+    <button @click="count++"> Increment </button>
+    <button @click="count--"> Decrement </button>
+</template>
+
+<style scoped>
+
+</style>
+
