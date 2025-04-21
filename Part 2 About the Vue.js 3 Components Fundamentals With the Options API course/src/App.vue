eae4a9fb0936e0cf0a3635bbbb87e13582d4cbd0
warisfullstack <146804289+warisfullstack@users.noreply.github.com>
2025-04-21 16:00:40 +0500

    Reuseable compoennt

--- Part 2 About the Vue.js 3 Components Fundamentals With the Options API course/src/App.vue
+++ Part 2 About the Vue.js 3 Components Fundamentals With the Options API course/src/App.vue
@@ -1,10 +1,17 @@
 <script setup>
 import ClickCounter from './components/ClickCounter.vue'
+import PlanComponent from './components/PlanComponent.vue'
+
 </script>
 
 <template>
   <h1>Hello World</h1>
   <ClickCounter />
+
+  <PlanComponent name="The curious" />
+  <PlanComponent name="The creator" />
+  <PlanComponent name="The explorer" />
+
 </template>
 
 <style scoped>
---
+++ Part 2 About the Vue.js 3 Components Fundamentals With the Options API course/src/components/PlanComponent.vue
@@ -0,0 +1,39 @@
+<script setup>
+const props = defineProps(
+    [
+        'name'
+    ]
+);
+</script>
+
+<template>
+    <div class="plan">
+        <div class="description">
+            <span class="title">
+                {{ name }}
+            </span>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.plan {
+  background-color: white;
+  padding: 15px;
+  margin-bottom: 15px;
+  border-radius: 8px;
+  cursor: pointer;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  transition: transform 0.2s;
+}
+
+.plan:hover {
+  transform: translateY(-3px);
+}
+
+.title {
+  font-weight: bold;
+  color: #333;
+  font-size: 1.1rem;
+}
+</style>
